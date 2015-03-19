1000a9ea510cb00ccbaf21d2e2e06dae7caae78f
gcanti <giulio.canti@gmail.com>
2015-03-19 16:35:03 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  locale: require('./lib/locale');
+};
---
+++ test/locale.js
@@ -0,0 +1,76 @@
+'use strict';
+
+var test = require('tape');
+var locale = require('../lib/locale');
+var t = require('tcomb');
+
+var Language = t.enums.of('it en', 'Language');
+var Country = t.enums.of('IT US', 'Country');
+var Locale = locale(Language, Country);
+var it_IT = new Locale({language: 'it', country: 'IT'});
+var en_US = new Locale({language: 'en', country: 'US'});
+
+test('locale', function (tape) {
+
+  tape.test('fields', function (tape) {
+    tape.plan(2);
+
+    var locale = new Locale({language: 'it', country: 'IT'});
+
+    tape.strictEqual(
+      locale.language,
+      'it',
+      'should have a language field');
+
+    tape.strictEqual(
+      locale.country,
+      'IT',
+      'should have a language field');
+
+  });
+
+  tape.test('getLanguageName', function (tape) {
+    tape.plan(2);
+
+    Locale.addLanguages({
+      it: {
+        it: 'Italiano',
+        en: 'Inglese'
+      }
+    });
+
+    tape.strictEqual(
+      it_IT.getLanguageName(),
+      'Italiano',
+      'should return the base language name');
+
+    tape.strictEqual(
+      it_IT.getLanguageName(en_US.language),
+      'Inglese',
+      'should return the language name');
+
+  });
+
+  tape.test('getCountryName', function (tape) {
+    tape.plan(2);
+
+    Locale.addCountries({
+      it: {
+        IT: 'Italia',
+        US: 'Stati Uniti'
+      }
+    });
+
+    tape.strictEqual(
+      it_IT.getCountryName(),
+      'Italia',
+      'should return the base language name');
+
+    tape.strictEqual(
+      it_IT.getCountryName(en_US.country),
+      'Stati Uniti',
+      'should return the language name');
+
+  });
+
+});
